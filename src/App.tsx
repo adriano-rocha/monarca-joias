ed144deed863f93c9109816527d9bdf208c54fb6
Adriano Rocha <adrianorocha.dev@gmail.com>
2026-03-30 18:36:29 -0300

    feat: configuração do i18n com suporte a PT, EN e ES

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { useTranslation } from 'react-i18next'
 import './index.css'
 
 function App() {
+  const { t } = useTranslation()
+
   return (
     <main>
       <h1 className="text-3xl font-bold text-yellow-500">
-        Monarca Jóias 👑
+        {t('hero.title')}
       </h1>
     </main>
   )
